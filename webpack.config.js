5c4d91717ddc4fc466b18608fb7f924b9cd4c3b9
Mateusz Pokora <m.pokora@binarapps.com>
2016-09-27 21:55:00 +0200

    app init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+let path = require('path');
+
+const ROOT_PATH = path.resolve(__dirname);
+const NODE_MODULES_PATH = path.resolve(__dirname, 'node_modules');
+
+const config = {
+  entry: './src/app.js',
+  resolve: {
+    extensions: ['', '.jsx', '.js']
+  },
+  output: {
+    path: './dist',
+    filename: 'app.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: [NODE_MODULES_PATH],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
