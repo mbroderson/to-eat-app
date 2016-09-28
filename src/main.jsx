1acafcb130813471f7fad61a9905e1925311ad82
Mateusz Pokora <m.pokora@binarapps.com>
2016-09-28 09:29:26 +0200

    separate toeat container

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,12 @@
 import React from 'react';
-import update from 'react-addons-update';
-import ToEatList from './toeat-list';
-import ToEatForm from './toeat-form';
-
-const toeats = [
-  {name: 'pasta', eaten: false},
-  {name: 'burger', eaten: false},
-  {name: 'szwajcar', eaten: false}
-];
+import ToEatContainer from './toeat-container';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {toeats: toeats};
-    this._addToEat = this._addToEat.bind(this);
-    this._checkItem = this._checkItem.bind(this);
-  }
-
-  _addToEat(toeatName) {
-    const newToeat = {name: toeatName, eaten: false};
-
-    this.setState({
-      toeats: [...this.state.toeats, newToeat]
-    });
-  }
-
-  _checkItem(index) {
-    const {toeats} = this.state;
-    const newItem = {...toeats[index], eaten: !toeats[index].eaten};
-
-    const newToeats = update(this.state.toeats, {$splice: [[index, 1, newItem]]});
-    this.setState({toeats: newToeats});
-  }
-
   render () {
     return (
       <div>
         <h1>To Eat App</h1>
-        <ToEatForm onSubmit={this._addToEat} />
-        <ToEatList toeats={this.state.toeats} onCheckItem={this._checkItem} />
+        <ToEatContainer />
       </div>
     );
   }
